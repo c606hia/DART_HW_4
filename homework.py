9f4b5eeca81c941650f58b4ceede957b225a6ccb
c606hia <77021063+c606hia@users.noreply.github.com>
2021-08-21 19:49:58 +0800

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,29 @@
+from pandas import read_csv
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+from sklearn.model_selection import RepeatedStratifiedKFold
+from skopt import BayesSearchCV
+
+
+
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/ionosphere.csv'
+dataframe = read_csv(url, header=None)
+
+data = dataframe.values
+X, y = data[:, :-1], data[:, -1]
+print(X.shape, y.shape)
+# define search space
+params = dict()
+params['C'] = (1e-6, 100.0, 'log-uniform')
+params['gamma'] = (1e-6, 100.0, 'log-uniform')
+params['degree'] = (1,5)
+params['kernel'] = ['linear', 'poly', 'rbf', 'sigmoid']
+# define evaluation
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# define the search
+search = BayesSearchCV(estimator=SVC(), search_spaces=params, n_jobs=-1, cv=cv)
+# perform the search
+search.fit(X, y)
+#best result
+print(search.best_score_)
+print(search.best_params_)
